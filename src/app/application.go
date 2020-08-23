27d119e420cbeffef7fe48a358bd5b2a05875eda
Pablo Mendez <mendez.developer@gmail.com>
2020-08-23 12:11:43 -0300

    added cassandra db config and implementation

--- src/app/application.go
+++ src/app/application.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/mendezdev/bookstore_oauth-api/src/clients/cassandra"
 	"github.com/mendezdev/bookstore_oauth-api/src/doamin/access_token"
 	"github.com/mendezdev/bookstore_oauth-api/src/http"
 	"github.com/mendezdev/bookstore_oauth-api/src/repository/db"
@@ -12,8 +13,15 @@ var (
 )
 
 func StartApplication() {
+	session, dbErr := cassandra.GetSession()
+	if dbErr != nil {
+		panic(dbErr)
+	}
+	session.Close()
 	atHandler := http.NewHandler(access_token.NewService(db.NewRepository()))
 
 	router.GET("/oauth/access_token/:access_token_id", atHandler.GetById)
+	router.POST("/oauth/access_token", atHandler.Create)
+
 	router.Run(":8080")
 }
